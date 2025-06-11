18cb4b18c26a04924b52ec3b2bb8713913c4e430
isalim079 <salimreza1025@gmail.com>
2025-06-11 11:48:19 +0600

    Featured recipe responsive done

--- src/components/Home/FeaturedRecipe/FeaturedRecipe.tsx
+++ src/components/Home/FeaturedRecipe/FeaturedRecipe.tsx
@@ -17,34 +17,33 @@ const FeaturedRecipe = () => {
   };
 
   return (
-    <div className=" font-poppins py-16 lg:py-20 px-6 lg:px-0">
+    <div className=" font-poppins py-12 lg:py-20 px-6 lg:px-0">
       <div className="max-w-screen-xl mx-auto">
         <div>
-          <h1 className="lg:text-5xl text-2xl font-bold text-center text-gray-800 font-cherrySwash">
-            Taste the Best – Handpicked Recipes for You
+          <h1 className="lg:text-5xl text-2xl font-bold text-gray-800 font-cherrySwash">
+            Taste the Best Handpicked Recipes for You
           </h1>
-          <p className="lg:text-xl lg:mt-4 text-center text-gray-800">
+          <p className="lg:text-xl lg:mt-4 text-gray-800 mt-3">
             Explore trending and highly-rated recipes from our vibrant cooking
             community.
           </p>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-14 mt-16">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-14 mt-8 lg:mt-16">
           {featuredRecipeData?.map((item, index) => (
             <div
               key={index}
-              className={`grid grid-cols-8 gap-5 items-center bg-white  p-7 rounded-md cursor-pointer shadow-[16px_17px_10px_-8px_rgba(0,_0,_0,_0.35)] border border-gray-300 ${
+              className={`grid grid-cols-8 gap-5 items-center bg-white p-6  lg:p-7 rounded-md cursor-pointer shadow-[16px_17px_10px_-8px_rgba(0,_0,_0,_0.35)] border border-gray-300 ${
                 isHovered === index &&
                 "-translate-y-3 transition-all duration-300 ease-in-out"
               }`}
-             
               onMouseEnter={() => handleMouseEnter(index)}
               onMouseLeave={handleMouseLeave}
             >
               <div className="lg:col-span-6 col-span-8">
-                <h1 className="text-gray-800 font-bold font-cherrySwash lg:text-xl mb-4">
+                <h1 className="text-gray-800 font-bold font-cherrySwash text-lg lg:text-xl mb-2 lg:mb-4">
                   {item.name}
                 </h1>
-                <p className=" text-gray-800">{item.description}</p>
+                <p className="text-gray-800">{item.description}</p>
                 <Lottie
                   className="w-14 mt-4 hidden lg:block"
                   animationData={anim}
